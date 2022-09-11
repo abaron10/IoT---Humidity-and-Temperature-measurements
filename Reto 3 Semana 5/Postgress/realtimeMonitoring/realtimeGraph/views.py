5559dea3390467d6d3e5e09d824b0de317ea0d40
andres.baron <andres.baron@rappi.com>
2022-09-11 12:24:13 -0500

    base ranges in posgres impl

--- Reto 3 Semana 5/Postgress/realtimeMonitoring/realtimeGraph/views.py
+++ Reto 3 Semana 5/Postgress/realtimeMonitoring/realtimeGraph/views.py
@@ -423,93 +423,9 @@ class RemaView(TemplateView):
 
         measureParam = self.kwargs.get('measure', None)
         isRange = self.request.GET.get('is_range', 0)
-        selectedMeasure = None
-        measurements = Measurement.objects.all()
-
-        if measureParam != None:
-            selectedMeasure = Measurement.objects.filter(name=measureParam)[0]
-        elif measurements.count() > 0:
-            selectedMeasure = measurements[0]
-
-        locations = Location.objects.all()
-       
-        def fromDates():
-            try:
-                start = datetime.fromtimestamp(
-                        float(self.request.GET.get('from', None))/1000)
-            except:
-                start = None
-            try:
-                end = datetime.fromtimestamp(
-                    float(self.request.GET.get('to', None))/1000)
-            except:
-                end = None
-            if start == None and end == None:
-                start = datetime.now()
-                start = start - \
-                    dateutil.relativedelta.relativedelta(
-                        weeks=1)
-                end = datetime.now()
-                end += dateutil.relativedelta.relativedelta(days=1)
-            elif end == None:
-                end = datetime.now()
-            elif start == None:
-                start = datetime.fromtimestamp(0)
-            return [start,end]          
-
-        avoidtime = False
-        start , end = None, None
-        startRange = 0
-        endRange = 0
-        print(f"{isRange}")
-        if isRange == "0":
-            start, end = fromDates()
-        else:
-            avoidtime = True
-            startRange = float(self.request.GET.get('start_range', 0))
-            endRange = float(self.request.GET.get('end_range', 0))
-
-        data = []
-        print(f"start es {startRange}, end es {endRange}")
-        for location in locations:
-            stations = Station.objects.filter(location=location)
-            
-            if not avoidtime:
-                locationData = Data.objects.filter(
-                    station__in=stations, measurement__name=selectedMeasure.name,  time__gte=start.date(), time__lte=end.date())
-            else:
-                locationData = Data.objects.filter(
-                    station__in=stations, measurement__name=selectedMeasure.name, time__gte=start.date(), time__lte=end.date())
-
-            if locationData.count() <= 0:
-                continue
-            minVal = locationData.aggregate(
-                Min('value'))['value__min']
-            maxVal = locationData.aggregate(
-                Max('value'))['value__max']
-            avgVal = locationData.aggregate(
-                Avg('value'))['value__avg']
-            data.append({
-                'name': f'{location.city.name}, {location.state.name}, {location.country.name}',
-                'lat': location.lat,
-                'lng': location.lng,
-                'population': stations.count(),
-                'min': minVal if minVal != None else 0,
-                'max': maxVal if maxVal != None else 0,
-                'avg': round(avgVal if avgVal != None else 0, 2),
-            })
-
-        startFormatted = start.strftime("%d/%m/%Y") if start != None else " "
-        endFormatted = end.strftime("%d/%m/%Y") if end != None else " "
-
-        context['measurements'] = measurements
-        context['selectedMeasure'] = selectedMeasure
-        context['locations'] = locations
-        context['start'] = startFormatted
-        context['end'] = endFormatted
-        context['data'] = data
-
+        context["result"] = isRange
         return context
+        
 
 
 """
